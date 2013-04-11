7c2b31427c6bd2a0a598b76dd7cda50d895daf18
Jim Mainprice <mainprice@gmail.com>
2013-04-11 18:20:02 -0400

    rename and initial import of model

--- examples/example0.py
+++ examples/example0.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Copyright (c) 2013 Worcester Polytechnic Institute
 #   Author: Jim Mainrpice <jmainprice@wpi.edu>
 #
@@ -27,15 +28,12 @@
 #   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # -*- coding: utf-8 -*-
 
-"""
-Created on Tue Apr  9 15:28:16 2013
-
-@author: jmainpri
-"""
-
 from openravepy import *
 import os
 import sys 
+from numpy import *
+from TransformMatrix import *
+from rodrigues import *
 
 #print "HOME : " + os.environ['HOME']
 #print "PYTHONPATH : " + os.environ['PYTHONPATH']
@@ -46,16 +44,30 @@ orEnv.SetDebugLevel(DebugLevel.Verbose)
 orEnv.SetViewer('qtcoin')
 orEnv.Reset()
 
-sys.stdin.readline()
+#Tcamera = array(((0.84028,  -0.14715,   0.52179,0.930986),
+#                 (0.52639,   0.45182,  -0.72026,-1.233453),
+#                 (-0.12976,   0.87989,   0.45711,2.412977)
 
-print "Try to create problem"
+#0.262839 -0.733602 -0.623389 0.0642694 2.99336 -0.755646 2.81558
+
+#T = MakeTransform(yaw_pitch_roll_rotation([0,0,0]),transpose(matrix([1,0,0])))
+#orEnv.GetViewer().SetCamera([0.262839 -0.733602 -0.623389 0.0642694 2.99336 -0.755646 2.81558])
+#sys.stdin.readline()
+
+print "draw frame"
+T = MakeTransform(eye(3),transpose(matrix([0,0,0]))) 
+h = misc.DrawAxes( orEnv, matrix(T), 1 )
+
+print "try to create problem"
 prob = RaveCreateProblem(orEnv,'Kinect')
 cmdout = prob.SendCommand('load data/lab1.env.xml')
 
-if cmdout is None:
-    raveLogWarn('command failed!')
-else:
-    cmdout = prob.SendCommand('numbodies')
-    print 'number of bodies are: ',cmdout
+#if cmdout is None:
+#    raveLogWarn('command failed!')
+#else:
+#    cmdout = prob.SendCommand('numbodies')
+#    print 'number of bodies are: ',cmdout
+  
     
 sys.stdin.readline()
+
