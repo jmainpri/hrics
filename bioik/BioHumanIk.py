2d24494472c1bbd0549621626f75a42942df8de3
Jim Mainprice <mainprice@gmail.com>
2014-08-05 16:37:59 -0400

    Add liscence

--- bioik/BioHumanIk.py
+++ bioik/BioHumanIk.py
@@ -1,3 +1,23 @@
+#
+# Copyright (c) 2014 WPI/ARC
+# All rights reserved.
+#
+# Permission to use, copy, modify, and distribute this software for any purpose
+# with or without   fee is hereby granted, provided   that the above  copyright
+# notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
+# PERFORMANCE OF THIS SOFTWARE.
+#
+#                                            Jim Mainprice on Tuesday Aug 5 2014
+
+# Code adapted from KAREN TROY's code
+
 from math_utils import *
 from numpy import *
 from numpy import linalg as la
