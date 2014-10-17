29b280736c3ff0f3ba1d463875d729a82862e579
Jim Mainprice <mainprice@gmail.com>
2014-10-17 14:27:55 -0400

    Cleanup play trajectory

--- python_module/bioik/play_file.py
+++ python_module/bioik/play_file.py
@@ -7,8 +7,6 @@ import time
 from copy import deepcopy
 import csv
 
-mapping = [-1, 6, 7, 8, 16, 17, 18, 20, 21, 22, 24, 25, 26]
-
 class PlayFile():
 
     def __init__(self):
@@ -25,35 +23,31 @@ class PlayFile():
         self.traj_human2 = []
         self.offset_pelvis_torso_init = self.humans[0].GetJoint("TorsoX").GetHierarchyChildLink().GetTransform()[0:3, 3]
 
-        t_cam = array([[ -0.655253290114, -0.106306078558, 0.747891799297, -0.302201271057] , \
-                        [ -0.725788890663, 0.363116971923, -0.584274379801, 2.68592453003] , \
-                        [ -0.209460287369, -0.925659269042, -0.315089361376, 2.25037527084] , \
+        t_cam = array([[ -0.662516847837, 0.365861186797, -0.653618404214, 3.09212255478] , \
+                        [ 0.748220341461, 0.282254919974, -0.600415256947, 2.43832302094] , \
+                        [ -0.0351816281021, -0.886835809012, -0.46074342198, 2.15959310532] , \
                         [ 0.0, 0.0, 0.0, 1.0]])
         self.env.GetViewer().SetCamera(t_cam)
 
 
-    def PrintView(self):
+    def print_view(self):
 
-        while True:
+        print "GetViewer().GetCameraTransform() : "
+        t_cam = self.env.GetViewer().GetCameraTransform()
 
-            print "GetViewer().GetCameraTransform() : "
-            t_cam = self.env.GetViewer().GetCameraTransform()
+        line_str = ""
+        line_str += 'array(['
+        for i in range(4):
+            line_str += '['
+            for j in range(4):
+                line_str += ' ' + str(t_cam[i, j]) + ','
+            line_str = line_str.rstrip(',')
+            line_str += ']'
+            if i < 3:
+                line_str += ' , \\ \n'
+        line_str += '])'
 
-            line_str = ""
-            line_str += 'array(['
-            for i in range(4):
-                line_str += '['
-                for j in range(4):
-                    line_str += ' ' + str(t_cam[i, j]) + ','
-                line_str = line_str.rstrip(',')
-                line_str += ']'
-                if i < 3:
-                    line_str += ' , \\ \n'
-            line_str += '])'
-
-            print line_str
-            print "Press return to get view matrix."
-            sys.stdin.readline()
+        print line_str
 
 
     def load_files(self, h1_filepath, h2_filepath):
@@ -74,26 +68,17 @@ class PlayFile():
     def play_skeleton(self):
         # for frame in self.frames:
         print len(self.traj_human1)
-        prev_time = self.traj_human1[0][0]
+
+        scale = 2.
 
         for row1, row2 in zip(self.traj_human1, self.traj_human2):
 
-            curr_time = row1[0]
-            dt = curr_time - prev_time
-            prev_time = curr_time
-            #print dt
-            time.sleep(0.05)
+            time.sleep(row1[0]*scale)
 
             del self.handles[:]
            
             self.humans[0].SetDOFValues(row1[1:self.humans[0].GetDOF()+1])
             self.humans[1].SetDOFValues(row2[1:self.humans[1].GetDOF()+1])
-
-            # if i % 3 == 0:
-            #     print "press enter to continue"
-            #     sys.stdin.readline()
-            #for h in self.humans:
-            #    print "robot in collision ", h.CheckSelfCollision()
 
     def run(self):
 
@@ -112,9 +97,10 @@ if __name__ == "__main__":
     print "try to load file : ", h2_file
 
     test = PlayFile()
+    test.load_files(h1_file, h2_file)
 
     while True:
-        test.load_files(h1_file, h2_file)
         test.play_skeleton()
+        test.print_view()
         print "press enter to exit"
         sys.stdin.readline()
